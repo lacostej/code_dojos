b284a57be08aa1ec9a1953effcb386b93f29a51e
jerome.lacoste <jerome.lacoste@gmail.com>
2009-11-26 19:52:51 +0100

    adeliverablesolution

--- 20091126-codedojo-tennis/during_event/tests/test_game.py
+++ 20091126-codedojo-tennis/during_event/tests/test_game.py
@@ -7,68 +7,70 @@ class TestTennisGame(unittest.TestCase):
     self.g = TennisGame()
 
   def testStartGameWithEmptyScore(self):
-    assertEquals("love-all", self.g.announceScore())
+    self.assertAnnouncement("love-all")
 
   def testThatSinglePointIsAnnounced(self):
     self.scorePlayerOne(1)
-    assertEquals("fifteen-love", self.g.announceScore())
+    self.assertAnnouncement("fifteen-love")
 
   def testThatSecondPointIsAnnounced(self):
     self.scorePlayerOne(2)
-    assertEquals("thirty-love", self.g.announceScore())
+    self.assertAnnouncement("thirty-love")
 
   def testThatPlayerTwoCanScore(self):
     self.scorePlayerTwo(2)
-    assertEquals("love-thirty", self.g.announceScore())
+    self.assertAnnouncement("love-thirty")
 
   def testThatBothPlayersScoreTwice(self):
     self.scorePlayerOne(2)
     self.scorePlayerTwo(2)
-    assertEquals("thirty-all", self.g.announceScore())
+    self.assertAnnouncement("thirty-all")
     
   def testThatDeuceIsAnnounced(self):
     self.scorePlayerOne(3)
     self.scorePlayerTwo(3)
-    assertEquals("deuce", self.g.announceScore())
+    self.assertAnnouncement("deuce")
 
   def testThatPlayersCanScoreThrice(self):
     self.scorePlayerOne(3)
-    assertEquals("forty-love", self.g.announceScore())
+    self.assertAnnouncement("forty-love")
 
   def testThatPlayerOneWinsGameAfterFourPoints(self):
     self.scorePlayerOne(4)
-    assertEquals("game player one", self.g.announceScore())
+    self.assertAnnouncement("game player one")
 
   def testThatPlayerOneGetsAdvantage(self):
     self.scorePlayerOne(3)
     self.scorePlayerTwo(3)
     self.scorePlayerOne(1)
-    assertEquals("advantage player one", self.g.announceScore())
+    self.assertAnnouncement("advantage player one")
 
   def testThatPlayerTwoWinsAfterDeuce(self):
     self.scorePlayerOne(3)
     self.scorePlayerTwo(5)
-    assertEquals("game player two", self.g.announceScore())
+    self.assertAnnouncement("game player two")
 
   def testThatPlayerTwoGetsAdvantage(self):
     self.scorePlayerTwo(3)
     self.scorePlayerOne(3)
     self.scorePlayerTwo(1)
-    assertEquals("advantage player two", self.g.announceScore())
+    self.assertAnnouncement("advantage player two")
 
   def testThatPlayerTwoGetsAdvantageAfterPlayerOneGetsAdvantage(self):
     self.scorePlayerTwo(3)
     self.scorePlayerOne(4)
     self.scorePlayerTwo(2)
-    assertEquals("advantage player two", self.g.announceScore())
+    self.assertAnnouncement("advantage player two")
 
   def testThatSecondDeuceIsAnnounced(self):
     self.scorePlayerTwo(3)
     self.scorePlayerOne(4)
     self.scorePlayerTwo(2)
     self.scorePlayerOne(1)
-    assertEquals("deuce", self.g.announceScore())
+    self.assertAnnouncement("deuce")
  
+  def assertAnnouncement(self,expectedAnnouncement):
+    assertEquals(expectedAnnouncement,self.g.announceScore())
   
   def scorePlayerOne(self, times):
     for each in range(times):
